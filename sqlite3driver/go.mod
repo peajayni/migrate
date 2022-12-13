f425a3dbf949576b2264b116791f248544d27fd6
peajayni <pdj1991@gmail.com>
2022-12-13 22:45:00 +0000

    added sqlite3 driver

---
+++ sqlite3driver/go.mod
@@ -0,0 +1,10 @@
+module github.com/peajayni/migrate/sqlite3driver
+
+go 1.19
+
+require (
+	github.com/hashicorp/go-multierror v1.1.1
+	github.com/peajayni/migrate v0.0.1
+)
+
+require github.com/hashicorp/errwrap v1.0.0 // indirect
---
+++ sqlite3driver/sqlite3driver.go
@@ -0,0 +1,104 @@
+package sqlite3driver
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/peajayni/migrate"
+)
+
+func NewSqlite3Driver(db *sql.DB, logger migrate.Logger) *Sqlite3Driver {
+	return &Sqlite3Driver{
+		db:     db,
+		logger: logger,
+	}
+}
+
+type Sqlite3Driver struct {
+	db     *sql.DB
+	logger migrate.Logger
+}
+
+func (d *Sqlite3Driver) Setup() error {
+	d.logger.Info("Ensuring schema_migration table exists")
+	query := `
+	create table if not exists schema_migration(
+		name string not null primary key,
+		executed_at timestamp not null default current_timestamp
+	)
+	`
+	_, err := d.db.Exec(query)
+	if err != nil {
+		return err
+	}
+	d.logger.Info("schema_migration table exists")
+	return nil
+}
+
+func (d *Sqlite3Driver) Applied() ([]string, error) {
+	d.logger.Info("Getting applied migrations")
+	query := `
+	select name
+	from schema_migration
+	order by executed_at asc
+	`
+	rows, err := d.db.Query(query)
+	if err != nil {
+		return []string{}, err
+	}
+	defer rows.Close()
+
+	var names []string
+	for rows.Next() {
+		var name string
+		err := rows.Scan(&name)
+		if err != nil {
+			return []string{}, err
+		}
+		names = append(names, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		return []string{}, err
+	}
+	d.logger.Info(fmt.Sprintf("Got %d applied migrations", len(names)))
+
+	return names, nil
+}
+
+func (d *Sqlite3Driver) Apply(migration migrate.Migration) error {
+	d.logger.Info(fmt.Sprintf("Applying migration: %s", migration.Name))
+	tx, txErr := d.db.Begin()
+	if txErr != nil {
+		return txErr
+	}
+	_, execErr := tx.Exec(migration.SQL)
+	if execErr != nil {
+		errRollback := tx.Rollback()
+		if errRollback != nil {
+			execErr = multierror.Append(execErr, errRollback)
+		}
+		return execErr
+	}
+
+	query := `
+		insert into schema_migration(name)
+		values (?)
+	`
+	_, execErr = tx.Exec(query, migration.Name)
+	if execErr != nil {
+		errRollback := tx.Rollback()
+		if errRollback != nil {
+			execErr = multierror.Append(execErr, errRollback)
+		}
+		return execErr
+	}
+
+	commitErr := tx.Commit()
+	if commitErr != nil {
+		return commitErr
+	}
+	d.logger.Info(fmt.Sprintf("Applied migration: %s", migration.Name))
+	return nil
+}
